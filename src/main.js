42acefb843efb284b51ccd2940f166583d63612e
Lakshamana <guitrompa1@gmail.com>
2019-12-26 22:59:17 -0300

    add global component

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+
+Vue.component('vc-component', {
+  data: function() {
+    return {
+      title: 'Curso de VueJS',
+      ctr: 0
+    }
+  },
+  methods: {
+    incrementar() {
+      this.ctr++
+    }
+  },
+  template: `
+    <div>
+      <h1>{{ title }}</h1>
+      <button @click="incrementar">Clicado {{ ctr }} vezes</button>
+    </div>
+  `
+})
+
+new Vue({
+  el: '#app'
+})
