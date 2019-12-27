8ac17fd4d6bb8c2af36c14b60c2190ea1f2a84dd
Lakshamana <guitrompa1@gmail.com>
2019-12-27 00:03:29 -0300

    add new components

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <AppContador/>
+</template>
+
+<script>
+import AppContador from './components/AppContador'
+
+export default {
+    components: {
+        AppContador
+    }    
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,48 +1,6 @@
 import Vue from 'vue'
-
-// Vue.component('vc-component', {
-//   data: function() {
-//     return {
-//       title: 'Curso de VueJS',
-//       ctr: 0
-//     }
-//   },
-//   methods: {
-//     incrementar() {
-//       this.ctr++
-//     }
-//   },
-//   template: `
-//     <div>
-//       <h1>{{ title }}</h1>
-//       <button @click="incrementar">Clicado {{ ctr }} vezes</button>
-//     </div>
-//   `
-// })
-
-const vcComponent = {
-  data: function() {
-    return {
-      title: 'Curso de VueJS',
-      ctr: 0
-    }
-  },
-  methods: {
-    incrementar() {
-      this.ctr++
-    }
-  },
-  template: `
-    <div>
-      <h1>{{ title }}</h1>
-      <button @click="incrementar">Clicado {{ ctr }} vezes</button>
-    </div>
-  `
-}
+import App from './App.vue'
 
 new Vue({
-  el: '#app',
-  components: {
-    'vc-component': vcComponent
-  }
-})
+  render: h => h(App)
+}).$mount('#app')
